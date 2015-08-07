35e429acccc745e5699943c221ae4f33d5942329
tshlee <tshlee@user-197.gradstudents.utoronto.ca>
2015-08-06 22:29:23 -0400

    update

--- latlong2map.py
+++ latlong2map.py
@@ -1,23 +1,27 @@
+import sys, os, ntpath
 from urllib import urlretrieve
 
+fstrIn = sys.argv[1]
+file=open(fstrIn,'r')
 lo=0;
-hi=29;
+hi=3;
 
 url_zoom="20"
 url_size="600x600"
 url_format="png"
 url_key="AIzaSyB87HHEk5A-fUfbfpQ0kzBXiZNTgPzbyzo"
 
-pstr = "images"
+pstrOut = sys.argv[2]
+if not os.path.exists(pstrOut):
+	os.makedirs(pstrOut)
 
-file=open("RV coordinates.txt",'r')
 for line in file.readlines()[lo:hi+1]:
 	s = line.split(',')
-
 	url_lat = s[0].strip()
 	url_long = s[1].strip()
 
-	url = "https://maps.googleapis.com/maps/api/staticmap?center=" +url_lat +"," +url_long +"&zoom=" +url_zoom +"&size=" +url_size +"&maptype=satellite&key=" +url_key +"&format=" +url_format
-	fstr = pstr + "/staticmap_%s_%s.%s" % (url_lat, url_long, url_format)
+	url = "https://maps.googleapis.com/maps/api/staticmap?center=%s,%s&zoom=%s&size=%s&maptype=%s&key=%s&format=%s" % (url_lat, url_long, url_zoom, url_size, "satellite", url_key, url_format)
+
+	fstr = "%s/%s_%s_%s.%s" % (pstrOut, ntpath.basename(pstrOut), url_lat, url_long, url_format)
 
 	urlretrieve(url, fstr)
